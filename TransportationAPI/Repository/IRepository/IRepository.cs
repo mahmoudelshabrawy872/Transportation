5c99cb863c73841e8d7836c88946fbc1e529549c
mahmoudelshabrawy872 <mahmoudelshabrawy872@gmail.com>
2023-05-12 16:08:42 +0300

    Add Documentation To Repository And IRepository

--- TransportationAPI/Repository/IRepository/IRepository.cs
+++ TransportationAPI/Repository/IRepository/IRepository.cs
@@ -2,13 +2,45 @@
 
 namespace TransportationAPI.Repository.IRepository
 {
+    /// <summary>
+    /// Generic repository interface that defines basic CRUD operations for a given entity type.
+    /// </summary>
+    /// <typeparam name="T">The entity type.</typeparam>
     public interface IRepository<T> where T : class
-
     {
+        /// <summary>
+        /// Retrieves all entities of type T from the database.
+        /// </summary>
+        /// <param name="filter">Optional filter expression to apply.</param>
+        /// <returns>A task representing the asynchronous operation that returns a collection of entities.</returns>
         Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+
+        /// <summary>
+        /// Retrieves a single entity of type T from the database.
+        /// </summary>
+        /// <param name="filter">Optional filter expression to apply.</param>
+        /// <param name="tracked">Determines whether the entity should be tracked by the context.</param>
+        /// <returns>A task representing the asynchronous operation that returns the first entity that matches the specified filter.</returns>
         Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true);
+
+        /// <summary>
+        /// Creates a new entity in the database.
+        /// </summary>
+        /// <param name="entity">The entity to create.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         Task CreateAsync(T entity);
+
+        /// <summary>
+        /// Deletes an entity from the database.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         Task DeleteAsync(T entity);
+
+        /// <summary>
+        /// Saves changes made in the context to the database.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation.</returns>
         Task SaveAsync();
     }
 }
--- TransportationAPI/Repository/Repository.cs
+++ TransportationAPI/Repository/Repository.cs
@@ -10,16 +10,21 @@ namespace TransportationAPI.Repository
         private protected ApplicationDbContext _context;
         internal DbSet<T> dbSet;
 
-
-
+        /// <summary>
+        /// Initializes a new instance of the Repository class.
+        /// </summary>
+        /// <param name="context">The application database context.</param>
         public Repository(ApplicationDbContext context)
         {
             _context = context;
             this.dbSet = _context.Set<T>();
         }
 
-
-
+        /// <summary>
+        /// Gets all entities of type T from the database.
+        /// </summary>
+        /// <param name="filter">Optional filter expression to apply.</param>
+        /// <returns>A collection of entities.</returns>
         public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
         {
             IQueryable<T> query = dbSet;
@@ -28,8 +33,12 @@ namespace TransportationAPI.Repository
             return query.ToList();
         }
 
-
-
+        /// <summary>
+        /// Gets a single entity of type T from the database.
+        /// </summary>
+        /// <param name="filter">Optional filter expression to apply.</param>
+        /// <param name="tracked">Determines whether the entity should be tracked by the context.</param>
+        /// <returns>The first entity that matches the specified filter.</returns>
         public async Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true)
         {
             IQueryable<T> query = dbSet;
@@ -42,18 +51,30 @@ namespace TransportationAPI.Repository
 
             return await query.FirstOrDefaultAsync();
         }
+
+        /// <summary>
+        /// Creates a new entity in the database.
+        /// </summary>
+        /// <param name="entity">The entity to create.</param>
         public async Task CreateAsync(T entity)
         {
             await dbSet.AddAsync(entity);
             await SaveAsync();
         }
 
+        /// <summary>
+        /// Deletes an entity from the database.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
         public async Task DeleteAsync(T entity)
         {
             dbSet.Remove(entity);
             await SaveAsync();
         }
 
+        /// <summary>
+        /// Saves changes made in the context to the database.
+        /// </summary>
         public async Task SaveAsync()
         {
             await _context.SaveChangesAsync();
